17a84b2303a14be7e5bf4a11b7971ae1567f3207
Leif Denby <leif@denby.eu>
2022-01-18 16:31:19 +0000

    test and introspection for all object metrics

--- cloudmetrics/objects/metrics/__init__.py
+++ cloudmetrics/objects/metrics/__init__.py
@@ -1,3 +1,6 @@
+import inspect
+import sys
+
 from ._object_properties import _get_regionprops
 from .geometry import (  # noqa
     max_length_scale,
@@ -13,3 +16,27 @@ def num_objects(object_labels):
     """
     regions = _get_regionprops(object_labels=object_labels)
     return len(regions)
+
+
+def _find_labelled_objects_functions():
+    """
+    Look through the functions available in this module and return all the ones
+    that look like they operate on labelled objects
+    """
+
+    def _takes_object_labels_kwarg(fn):
+        fn_sig = inspect.signature(fn)
+        return "object_labels" in fn_sig.parameters and len(fn_sig.parameters) == 1
+
+    fns = [
+        (fn_name, fn)
+        for (fn_name, fn) in inspect.getmembers(
+            sys.modules[__name__], inspect.isfunction
+        )
+        if not fn_name.startswith("_") and _takes_object_labels_kwarg(fn)
+    ]
+
+    return dict(fns)
+
+
+ALL_METRIC_FUNCTIONS = _find_labelled_objects_functions()
--- tests/test_labelled_objects.py
+++ tests/test_labelled_objects.py
@@ -1,3 +1,17 @@
 """
 Try running all functions for labelled objects directly on masks
 """
+import numpy as np
+import pytest
+
+from cloudmetrics.objects import label as label_objects
+from cloudmetrics.objects import metrics as obj_metrics
+
+
+@pytest.mark.parametrize("metric_name", obj_metrics.ALL_METRIC_FUNCTIONS.keys())
+def test_all_object_metrics_with_random_mask(metric_name):
+    N = 100
+    cloud_mask = np.random.random((N, N)) > 0.8
+    object_labels = label_objects(cloud_mask=cloud_mask)
+    fn = obj_metrics.ALL_METRIC_FUNCTIONS[metric_name]
+    fn(object_labels=object_labels)
