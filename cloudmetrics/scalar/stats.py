628c4b5131186089e84b89e2a8843f59d88eb109
martinjanssens <martinjanssens94@hotmail.com>
2022-01-23 15:30:03 +0100

    Added expanded docstrings to stats functions, made mask optional input argument to these functions and revised the stats tests to test both masked and unmasked fields for consistency with sp.stats and np

--- cloudmetrics/scalar/stats.py
+++ cloudmetrics/scalar/stats.py
@@ -1,29 +1,89 @@
 import scipy as sp
 
 
-def mean(mask, scalar_field):
+def mean(scalar_field, mask=None):
     """
-    Compute the masked mean of scalar field
+    Compute the (optionally masked) mean of scalar field
+
+    Parameters
+    ----------
+    scalar_field : numpy array of shape (npx,npx) - npx is number of pixels
+        Scalar for which to calculate the mean.
+    mask : Optional (Boolean) mask. If passed, the mean will be computed over the
+        masked (True) pixels.
+
+    Returns
+    -------
+    mean : float
+        Mean over the (masked) field
     """
-    return scalar_field[mask].mean()
+    if isinstance(mask, type(None)):
+        return scalar_field.mean()
+    else:
+        return scalar_field[mask].mean()
 
 
-def var(mask, scalar_field):
+def var(scalar_field, mask=None):
     """
-    Compute the masked variance of scalar field
+    Compute the (optionally masked) variance of scalar field
+
+    Parameters
+    ----------
+    scalar_field : numpy array of shape (npx,npx) - npx is number of pixels
+        Scalar for which to calculate the variance.
+    mask : Optional (Boolean) mask. If passed, the variance will be computed over the
+        masked (True) pixels.
+
+    Returns
+    -------
+    variance : float
+        Variance over the (masked) field
     """
-    return scalar_field[mask].var()
+    if isinstance(mask, type(None)):
+        return scalar_field.var()
+    else:
+        return scalar_field[mask].var()
 
 
-def skew(mask, scalar_field):
+def skew(scalar_field, mask=None):
     """
-    Compute the masked skewness of scalar field
+    Compute the (masked) skewness of scalar field
+
+    Parameters
+    ----------
+    scalar_field : numpy array of shape (npx,npx) - npx is number of pixels
+        Scalar for which to calculate the skewness.
+    mask : Optional (Boolean) mask. If passed, the skewness will be computed over the
+        masked (True) pixels.
+
+    Returns
+    -------
+    skewness : float
+        Skewness over the (masked) field
     """
-    return sp.stats.skew(scalar_field[mask])
+    if isinstance(mask, type(None)):
+        return sp.stats.skew(scalar_field)
+    else:
+        return sp.stats.skew(scalar_field[mask])
 
 
-def kurtosis(mask, scalar_field):
+def kurtosis(scalar_field, mask=None):
     """
-    Compute the masked kurtosis of scalar field
+    Compute the (masked) kurtosis of scalar field
+
+    Parameters
+    ----------
+    scalar_field : numpy array of shape (npx,npx) - npx is number of pixels
+        Scalar for which to calculate the kurtosis.
+    mask : Optional (Boolean) mask. If passed, the kurtosis will be computed over the
+        masked (True) pixels.
+
+    Returns
+    -------
+    kurtosis : float
+        Kurtosis over the (masked) field
     """
-    return sp.stats.kurtosis(scalar_field[mask])
+    if isinstance(mask, type(None)):
+        return sp.stats.kurtosis(scalar_field)
+    else:
+        return sp.stats.kurtosis(scalar_field[mask])
--- tests/test_scalar_stats.py
+++ tests/test_scalar_stats.py
@@ -26,22 +26,23 @@ def _generate_example(measure, N=10, Nc=10):
     scalar_field[i, j] = scalar_values
 
     fn = _get_stats_func(measure=measure)
-    value = fn(scalar_field[mask])
+    value_field = fn(scalar_field.flatten())
+    value_masked = fn(scalar_field[mask])
 
     # keep trying until we make a field where we don't get the same value for
-    # the applying the reduction the whole field
-    if fn(scalar_field.flatten()) == value:
+    # applying the reduction to the whole field and the masked field
+    if value_field == value_masked:
         return _generate_example(measure=measure, N=N, Nc=Nc)
 
-    return mask, scalar_field, value
+    return mask, scalar_field, value_field, value_masked
 
 
 @pytest.mark.parametrize("measure", "mean var skew kurtosis".split())
 def test_masked_statistic(measure):
-    mask, scalar_field, value_true = _generate_example(measure=measure)
+    mask, scalar_field, value_field_true, value_masked_true = _generate_example(
+        measure=measure
+    )
     fn_cm = getattr(cloudmetrics.scalar, measure)
-    value = fn_cm(mask, scalar_field=scalar_field)
-    assert value == value_true
 
-    fn = _get_stats_func(measure=measure)
-    assert value != fn(scalar_field.flatten())
+    assert fn_cm(scalar_field.flatten()) == value_field_true
+    assert fn_cm(scalar_field, mask=mask) == value_masked_true
