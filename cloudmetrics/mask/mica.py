20fa87c075150dd1364d368fd001cd093145679d
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2023-12-06 15:02:07 +0100

    linting fixes

--- cloudmetrics/mask/mica.py
+++ cloudmetrics/mask/mica.py
@@ -30,17 +30,16 @@ def mica(mask, periodic_domain=False):
     """
 
     image_size = mask.size
-    mask_binary = np.where(mask>0, 1, 0)
+    mask_binary = np.where(mask > 0, 1, 0)
     total_area = np.sum(mask_binary)
-
 
     if periodic_domain:
         doubled_mask = np.tile(mask_binary, (2, 2))
 
         indices_x = np.max(doubled_mask, axis=0)
         indices_y = np.max(doubled_mask, axis=1)
-        max_consecutive_zeros_x = max(map(len, ''.join(map(str, indices_x)).split('1')))
-        max_consecutive_zeros_y = max(map(len, ''.join(map(str, indices_y)).split('1')))
+        max_consecutive_zeros_x = max(map(len, "".join(map(str, indices_x)).split("1")))
+        max_consecutive_zeros_y = max(map(len, "".join(map(str, indices_y)).split("1")))
 
         delta_x = mask_binary.shape[0] - max_consecutive_zeros_x
         delta_y = mask_binary.shape[1] - max_consecutive_zeros_y
@@ -50,13 +49,11 @@ def mica(mask, periodic_domain=False):
 
         # Get the minimum and maximum coordinates along each axis
         min_x, min_y = np.min(non_zero_indices, axis=1)
-        max_x, max_y = np.max(non_zero_indices, axis=1)+1
+        max_x, max_y = np.max(non_zero_indices, axis=1) + 1
         delta_x = max_x - min_x
         delta_y = max_y - min_y
 
     Acls = delta_x * delta_y  # minimum rectangle including all convective regions
-    mica = 1.* ( total_area / Acls ) * ( image_size - Acls ) / image_size
-
+    mica = 1.0 * (total_area / Acls) * (image_size - Acls) / image_size
 
     return mica
-
